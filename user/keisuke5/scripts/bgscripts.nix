3935f022d9d50cabf9e7f26ed99e6af31fad1533
Karthik <kskarthik20025@gmail.com>
2023-12-15 17:36:29 +0530

    adding custom scripts

--- user/keisuke5/scripts/bgscripts.nix
+++ user/keisuke5/scripts/bgscripts.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
   pkgs.writeShellApplication = {
     name = "switchbg";
-    runtimeInputs = [ pkgs.feh ];
     text = ''
       cp  "$(find ~/Pictures/backgrounds -type f | shuf -n 1)" ~/.background-image
       feh --bg-fill ~/.background-image
