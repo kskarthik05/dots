1506aaf7d3b3fcb9503ae3fa6b802f69733dbf20
Karthik <kskarthik20025@gmail.com>
2023-12-15 17:29:53 +0530

    adding custom scripts

--- user/keisuke5/scripts/bgscripts.nix
+++ user/keisuke5/scripts/bgscripts.nix
@@ -1,12 +1,10 @@
-{ pkgs, ... }:{
-  pkgs.writeShellApplication {
-  name = "switchbg";
-
-  runtimeInputs = [ curl w3m ];
-
-  text = ''
-    cp  "$(find ~/Pictures/backgrounds -type f | shuf -n 1)" ~/.background-image
-    feh --bg-fill ~/.background-image
-  '';
+{ pkgs, ... }: {
+  pkgs.writeShellApplication = {
+    name = "switchbg";
+    runtimeInputs = [ pkgs.feh ];
+    text = ''
+      cp  "$(find ~/Pictures/backgrounds -type f | shuf -n 1)" ~/.background-image
+      feh --bg-fill ~/.background-image
+    '';
   };
 }
