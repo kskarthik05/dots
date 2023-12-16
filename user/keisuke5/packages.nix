fdff2031838d21350fa59fb176da1fcb6441ef4b
Karthik <kskarthik20025@gmail.com>
2023-12-16 14:04:16 +0530

    added bg scripts to home.packages

--- user/keisuke5/packages.nix
+++ user/keisuke5/packages.nix
@@ -11,6 +11,20 @@
     steamtinkerlaunch
     git
     nixfmt
+    (pkgs.writeShellScriptBin "switchbg" ''
+      cp  "$(find ~/Pictures/backgrounds -type f | shuf -n 1)" ~/.background-image
+      ${feh}/bin/feh --bg-fill ~/.background-image
+      dbus-send --session --dest=org.kde.plasmashell --type=method_call \
+      /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+      var Desktops = desktops();
+      for (i=0;i<Desktops.length;i++) {
+        d = Desktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+        d.writeConfig("Image", "file:///home/keisuke5/.background-image");
+      }'
+    '')
+
   ];
   programs.vscode = {
     enable = true;
