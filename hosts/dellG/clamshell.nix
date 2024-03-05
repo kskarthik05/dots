b1de0370121b6382fe953cfe4f0a072b37c396cc
Karthik <kskarthik20025@gmail.com>
2024-03-05 13:17:28 +0530

    removed clamshell spec

--- hosts/dellG/clamshell.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }: {
-  hardware.nvidia.prime.sync.enable = lib.mkForce false;
-  boot.kernelParams = [ "module_blacklist=i915" ];
-}
--- hosts/dellG/default.nix
+++ hosts/dellG/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [ ./base.nix ./hardware.nix ];
   specialisation = {
-    clamshell.configuration = { imports = [ ./clamshell.nix ]; };
     on-the-go.configuration = { imports = [ ./on-the-go.nix ]; };
     vfio.configuration = { imports = [ ./vfio.nix ]; };
     base-with-vm.configuration = { imports = [ ./base-with-vm.nix ]; };
