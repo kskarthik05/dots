1cac11e4db43c8abc97017e76db79842549ff233
Karthik <kskarthik20025@gmail.com>
2026-05-06 21:04:49 +0530

    FLAKE UPDATE

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Nice Flake";
   inputs = {
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, nixpkgs-unstable,  home-manager, ... }@inputs:
